1b8dfb1bd233bdcc07c29def9b45232b591f7b73
Guibrgs <guilhem.bourgeois@protonmail.com>
2019-09-12 10:25:37 +0200

    add TestBoite TestConstructor

---
+++ src/test/java/lego/test/TestBoite.java
@@ -0,0 +1,27 @@
+package lego.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import lego.BoiteLego;
+
+class TestBoite {
+
+	@Test
+	void testBoiteConstructor() {
+		// given
+		var number = 21318;
+		var name = "La cabane dans l'arbre";
+		// when
+		var boite = new BoiteLego(number, name);
+		// then
+		assertAll(
+				() -> assertEquals(number, boite.getNumber(), "number"),
+				() -> assertEquals(name, boite.getName(), "name")
+			);
+		
+	}
+	
+	
+}
