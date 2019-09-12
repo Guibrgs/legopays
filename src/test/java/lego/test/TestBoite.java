1a4a4ffafb8224f1d464b6de8e946548d73835e3
Guibrgs <guilhem.bourgeois@protonmail.com>
2019-09-12 11:34:03 +0200

    resolved TestTheme new TestConstructor for TestBoite

--- src/test/java/lego/test/TestBoite.java
+++ src/test/java/lego/test/TestBoite.java
@@ -13,12 +13,18 @@ class TestBoite {
 		// given
 		var number = 21318;
 		var name = "La cabane dans l'arbre";
+		var pieces = 325;
+		var figurines = 59;
+		var price = 10999;
 		// when
-		var boite = new Boite(number, name);
+		var boite = new Boite(number, name, pieces, figurines, price);
 		// then
 		assertAll(
 				() -> assertEquals(number, boite.getNumber(), "number"),
-				() -> assertEquals(name, boite.getName(), "name")
+				() -> assertEquals(name, boite.getName(), "name"),
+				() -> assertEquals(pieces, boite.getPieces(), "pieces"),
+				() -> assertEquals(figurines, boite.getFigurines(), "figurines"),
+				() -> assertEquals(price, boite.getPrice(), "price")
 			);
 		
 	}
--- src/test/java/lego/test/TestTheme.java
+++ src/test/java/lego/test/TestTheme.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import lego.Boite;
+import lego.Theme;
 
 class TestTheme {
 
